6dba2b2ec7d152bad912919ce09fe9740620d0e9
Hugo Marshall <hugomarshall@Hugos-MacBook-Pro.local>
2020-07-03 15:28:40 +1200

    links to different pages added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/loggedIn', function(req, res, next) {
+  res.render('loggedIn', { title: 'Express' });
 });
+router.get('/oggedOut', function(req, res, next) {
+  res.render('loggedOut', { title: 'Express' });
+});
+router.get('/logIn', function(req, res, next) {
+  res.render('logIn', { title: 'Express' });
+});
+router.get('/createAccount', function(req, res, next) {
+  res.render('createAccount', { title: 'Express' });
+});
+router.get('/accountSettings', function(req, res, next) {
+  res.render('accountSettings', { title: 'Express' });
+});
+router.get('/addChannel', function(req, res, next) {
+  res.render('addChannel', { title: 'Express' });
+});
+router.get('/editChannel', function(req, res, next) {
+  res.render('editChannel', { title: 'Express' });
+});
+router.get('/deleteChannel', function(req, res, next) {
+  res.render('deleteChannel', { title: 'Express' });
+});
+router.get('/addFriend', function(req, res, next) {
+  res.render('addFriend', { title: 'Express' });
+});
+
 
 module.exports = router;
